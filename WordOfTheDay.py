ce7c339dd9a8627c2b8095b243369f7f961fad1c
iancmckee <I-mckee@onu.edu>
2020-10-09 23:59:30 -0500

    Initial Commit - uploading WordOfTheDay.py
    Kicking things off with some good ol' first .py files

---
+++ WordOfTheDay.py
@@ -0,0 +1,146 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+import requests
+from requests import HTTPError
+from discord.utils import get
+import string
+import discord
+from discord.ext import commands
+from discord.errors import HTTPException
+import re
+import os
+
+client = commands.Bot(command_prefix="!")
+
+@client.event
+async def on_ready():
+    print("Word of the Day bot is ready")
+
+# @client.command()
+# async def word(message):
+
+# @client.command()
+# async def help(message):
+#
+# @client.command()
+# async def whaddup(message):
+#     await message.channel.send("Oh Whaddup")
+
+@client.event
+async def on_message(message):
+    try:
+        # if(str(message.author) != "Word of the Day#2026"):
+        #     command = message.content.lower()[0:message.content.find(" ")]
+        if message.content.lower()[0:5] == ('!help'):
+            await message.channel.send("I'm a super basic bot. here are your options.... \n \n"
+                                       "!help\t-->\tyou enetered this, you know this pulls up the help menu \n"
+                                       "!word\t-->\tthis command will provide a word of the day... \n"
+                                       "!define {Word or phrased to be defined} \t-->\t This command will define"
+                                       " a word or phrase you declare after the command.\n"
+                                       "!udefine {Word or phrased to be defined} \t-->\t This command"
+                                       " will define a word or phrase... but from Urban Dictionary"
+                                       "\n\n"
+                                       "That concludes the options")
+        if message.content.lower()[0:5] == ("!word"):
+            dictionarySite = "https://www.merriam-webster.com/word-of-the-day"
+            page_html = requests.get(dictionarySite, headers={'User-Agent': 'Mozilla/5.0'}).text
+            page_soup = soup(page_html, "html.parser")
+            word = page_soup.find("title")
+            literaryElement = page_soup.find("span", class_="main-attr")
+            pronunciation = page_soup.find("span", class_="word-syllables")
+            definitions = {}
+
+            for iteration,tag in enumerate(page_soup.find_all("p")):
+                if tag.text[0:1].isdigit():
+                    definitions[iteration] = tag.text
+
+            finalmessage = ""
+            finalmessage += word.string + "\n"
+            finalmessage += literaryElement.string + "\n"
+            finalmessage += "Definition(s): \n "
+            finalmessage += "Pronunciation: " + pronunciation.text + "\n"
+            for definition in definitions:
+                finalmessage += definitions[definition] + "\n"
+            finalmessage += dictionarySite
+            await message.channel.send(finalmessage)
+        if message.content.lower()[0:7] == "!define":
+            defineWord = message.content.lower()[7:].lstrip()
+            finalmessage = ""
+            if not defineWord:
+                finalmessage+= "Please include a word to be defined"
+            else:
+                site = "https://www.merriam-webster.com/dictionary/"+ defineWord.replace(" ", "%20") + "?src=search-dict-hed"
+                try:
+                    html_text = requests.get(site, headers={'User-Agent': 'Mozilla/5.0'}).text
+                    definitionSoup = soup(html_text, "html.parser")
+                    defins = definitionSoup.find_all("span", class_="dt")
+                    pronunciation = definitionSoup.find("span", class_="pr")
+                    literaryElement = definitionSoup.find("a", class_="important-blue-link")
+                    suggestions = definitionSoup.find("p", class_="spelling-suggestions")
+                    if literaryElement is not None:
+                        finalmessage += "Word: " +"**" + defineWord.upper() + "**" + "\t-\t" + literaryElement.text + "\n "
+                    if pronunciation is not None:
+                        finalmessage += "Pronunciation: " + pronunciation.text + "\n"
+                    for iteration, defin in enumerate(defins):
+                        if defin.text.strip() != "":
+                            finalmessage += re.sub(' +', ' ', defin.text.replace("\n", " - ").strip()) + "\n"
+                        if iteration ==2:
+                            break
+                    if suggestions is not None:
+                        finalmessage += "Could not find the word " + defineWord + " in the dictionary, you may have spelled it wrong,\n did you mean one of the following words?\n"
+                        suggestions = suggestions.text.replace(" ", ", ")
+                        finalmessage += suggestions + "\n"
+                    finalmessage += "Reference site: " + site
+                except HTTPError as err:
+                    if err.code == 404:
+                        finalmessage += "Could not find word in the dictionary,"
+                        await message.channel.send(finalmessage)
+                    else:
+                        await message.channel.send("Could not find word in the dictionary, but with style...")
+                except AttributeError as error:
+                    await message.channel.send("Something went wrong parsing the website for a Definition. Just saw it live Sry." + str(error))
+                except Exception as exception:
+                    await message.channel.send("Just a generic Exception... my bad.\n" + str(exception))
+            await message.channel.send(finalmessage)
+        if message.content.lower()[0:8]=="!udefine":
+            defineWord = message.content.lower()[8:].lstrip()
+            finalmessage = ""
+            if not defineWord:
+                site =""
+                finalmessage += "Please include a word to be defined"
+            else:
+                site = "https://www.urbandictionary.com/define.php?term=" + defineWord.replace(" ","%20")
+                try:
+                    #requests.headers{'User-Agent': 'Mozilla/5.0','content-length':'2000'}
+                    html_text = requests.get(site, headers={'User-Agent': 'Mozilla/5.0'}).text
+                    definitionSoup = soup(html_text, "html.parser")
+                    defins = definitionSoup.find_all("div", class_="meaning")
+                    examples = definitionSoup.find_all("div",class_="example")
+                    finalmessage += "**" + defineWord.upper() + "** - " + "\n"
+                    #get rid of the word of the day definition
+                    if defins:
+                        defins.pop(1)
+                    for iteration, defin in enumerate(defins):
+                        finalmessage += "\t: " + defin.text.replace("\n", "").strip() + "\n"
+                        if iteration == 2:
+                            break
+                except HTTPError as err:
+                    if err.code == 404:
+                        finalmessage += "Could not find word in the dictionary,"
+                        await message.channel.send(finalmessage)
+                    else:
+                        await message.channel.send("Could not find word in the dictionary, but with style...")
+                except AttributeError as error:
+                    await message.channel.send(
+                        "Something went wrong parsing the website for a Definition. Just saw it live Sry." + str(error))
+                except Exception as exception:
+                    await message.channel.send("Just a generic Exception... my bad.\n" + str(exception))
+            finalmessage+= site
+            await message.channel.send(finalmessage)
+        if message.content.lower()[0:16] == "!whosucksatchess" or message.content.lower()[0:6] == "!chess":
+            await message.channel.send("Harry sucks at Chess")
+    except Exception as exception:
+        await message.channel.send("Discord's API can't handle this many definitions, if you want to know the definitions to this word go to: \n" + site + "\n" + str(exception))
+
+
+os.environ("DISCORD_DICTIONARY_BOT_KEY")
